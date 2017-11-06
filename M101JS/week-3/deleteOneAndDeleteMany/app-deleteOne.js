99f67188196057a60bd6464ed3144b94b3dbf155
manish-giri <manish.giri.me@gmail.com>
2017-11-06 05:26:25 -0500

    Add data for M101JS Week 3

---
+++ M101JS/week-3/deleteOneAndDeleteMany/app-deleteOne.js
@@ -0,0 +1,51 @@
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+
+MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
+
+    assert.equal(err, null);
+    console.log("Successfully connected to MongoDB.");
+    
+    var query = {"permalink": {"$exists": true, "$ne": null}};
+    var projection = {"permalink": 1, "updated_at": 1};
+
+    var cursor = db.collection('companies').find(query);
+    cursor.project(projection);
+    cursor.sort({"permalink": 1})
+
+    var numToRemove = 0;
+
+    var previous = { "permalink": "", "updated_at": "" };
+    cursor.forEach(
+        function(doc) {
+
+            if ( (doc.permalink == previous.permalink) && (doc.updated_at == previous.updated_at) ) {
+                console.log(doc.permalink);
+
+                numToRemove = numToRemove + 1;
+
+                var filter = {"_id": doc._id};
+
+                db.collection('companies').deleteOne(filter, function(err, res) {
+
+                    assert.equal(err, null);
+                    console.log(res.result);
+
+                });
+
+            }
+            
+            previous = doc;
+            
+        },
+        function(err) {
+
+            assert.equal(err, null);
+
+        }
+    );
+
+});
+
+
